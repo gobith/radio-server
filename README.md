fbc9ecfea6ee72d9e84ecbfb77435d5753de2154
Georges <gkgobith@gmail.com>
2022-03-05 22:53:13 +0100

    daemons

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # radio-server
+
 This is the raspberry server part of the code
 It will serve the radio client
-And it will control the mpc 
+And it will control the mpc
+
+music player daemon
+sudo apt-get install mpd
+
+music player client
+sudo apt-get install mpc
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,48 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 import radio_list from "../data/radio_list.json";
+import { MPC } from "mpc-js";
 
 const app: Application = express();
 
-app.use(express.static('public'));
+const mpc = new MPC();
+mpc.connectTCP("localhost", 6600);
+mpc.currentPlaylist.clear();
+
+radio_list.forEach((item) => {
+  mpc.currentPlaylist.add(item.url);
+});
+
+app.use(express.static("public"));
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response, next: NextFunction): void => {
   res.send("hello from raspberry tada");
 });
 
+app.get("/current", (req: Request, res: Response, next: NextFunction): void => {
+  res.json(radio_list[2]);
+});
+
+app.post("/play", (req: Request, res: Response, next: NextFunction): void => {
+  console.log(req.body.station);
+
+  let index = radio_list.findIndex((item) => {
+    return item.station === req.body.station;
+  });
+
+  console.log(index);
+
+  if (index >= 0) {
+    mpc.playback.play(index);
+  }
+
+  res.json(index);
+});
+
 app.get("/list", (req: Request, res: Response, next: NextFunction): void => {
   res.json(radio_list);
 });
 
-app.listen(6000, () => {
+app.listen(5000, () => {
   console.log("server running");
 });
